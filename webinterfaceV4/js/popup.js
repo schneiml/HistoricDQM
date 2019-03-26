9dfbb1883c418fde6efdc90a71937b0dcc0cbaa8
andrius <andrius.kirilovas@cern.ch>
2019-03-26 17:30:20 +0100

    Added ability to change y range of plots dynamically

--- webinterfaceV4/js/popup.js
+++ webinterfaceV4/js/popup.js
@@ -1,15 +1,9 @@
 var current_chart = null;
 var popup_chart = null;
-// var last_popup_name = null;
 
 function popup(el) {
     var name = $(el).attr("name");
     console.log("popup: " + name);
-
-    // if (name === last_popup_name)
-    //     return;
-
-    // last_popup_name = name;
 
     if (popup_chart != null)
         Chart.prototype.destroy.call(popup_chart);
@@ -19,6 +13,9 @@ function popup(el) {
     $("#start").val(current_chart.runs_range[0]);
     $("#end").val(current_chart.runs_range[1]);
 
+    $("#start-y").val(current_chart.chart_obj.yAxis[0].min);
+    $("#end-y").val(current_chart.chart_obj.yAxis[0].max);
+    
     popup_chart = $.extend(true, new current_chart.constructor({}, ""), current_chart); //copy chart
 
     popup_chart.el_id = "popup-chart";
@@ -26,17 +23,23 @@ function popup(el) {
     popup_chart.last_filters = undefined;
     popup_chart.chart_obj = null;
     
-    console.log(popup_chart);
     Chart.prototype.update.call(popup_chart);
 }
 
 function updatePopup() {
     console.log("updatePopup()");
-    var range = [0,Infinity];
     var start = $("#start").val();
     var end = $("#end").val();
+    var start_y = $("#start-y").val();
+    var end_y = $("#end-y").val();
     
     popup_chart.filters.runs = new Filter(true, "run-filter-range", new NumRange(parseInt(start), parseInt(end)));
-    
+
     Chart.prototype.update.call(popup_chart);
+
+    // Update y range
+    popup_chart.chart_obj.yAxis[0].update({
+        min: start_y,
+        max: end_y
+    });
 }
